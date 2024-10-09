55ef6518180f1569006f5f9b11392ce693fc4eeb
unknown <bhuvaneswarim1210@gmail.com>
2024-10-09 21:02:33 +0530

    change10

--- src/main/java/org/snapshot/amazon/CrossBrowser_DockerTest.java
+++ src/main/java/org/snapshot/amazon/CrossBrowser_DockerTest.java
@@ -1,16 +1,13 @@
 package org.snapshot.amazon;
 
 import java.net.MalformedURLException;
-import java.net.URL;
-
+import java.net.URL; 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class CrossBrowser_DockerTest {
     WebDriver driver;
